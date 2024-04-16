3c1cbee0fb682caf483f25c15043192ea703d78f
kurokiyuki <saturnianpersonal@gmail.com>
2024-04-16 18:27:18 +0900

    updated 2.4.91

--- Packages/com.satania.kiseteneex/Editor/New 「キセテネ」改良版/NewKiseteneEx.cs
+++ Packages/com.satania.kiseteneex/Editor/New 「キセテネ」改良版/NewKiseteneEx.cs
@@ -8,7 +8,7 @@ namespace Saturnian_NewKiseteneEx_Package
     public partial class NewKiseteneEx : EditorWindow
     {
 
-        public static string version = "v2.4.9(VPM)";
+        public static string version = "v2.4.91(VPM)";
 
         /// <summary>
         /// 着せたい服
